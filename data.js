7f371fb000a7d989f117dabccdecafeaef671813
programmingashram <avenuedesign7658@gmail.com>
2023-01-20 07:48:29 +0530

    added

---
+++ data.js
@@ -0,0 +1,236 @@
+let CardDetails = {
+    Car :{
+        img : './img/washingdetailing.svg',
+        title : "Car Washing" ,
+        para : " Lorem iparasum dolor sit amet consectetur adiparaisicing elit."
+    },
+    wheel :{
+        img : 'https://vehiclecare.in/images/service/wheelcare.svg',
+        title : "wheel care" ,
+        para : " Lorem iparasum dolor sit amet consectetur adiparaisicing elit."
+    },
+    carac :{
+        img : 'https://vehiclecare.in/images/service/acrepair.svg',
+        title : "Ac repare" ,
+        para : " Lorem iparasum dolor sit amet consectetur adiparaisicing elit."
+    }, 
+    Battery :{
+        img :  'https://vehiclecare.in/images/service/batteriessupport.svg',
+        title : "Battery changing" ,
+        para : " Lorem iparasum dolor sit amet consectetur adiparaisicing elit."
+    },
+    Denting :{
+        img : 'https://vehiclecare.in/images/service/dentingpainting.svg',
+        title : "Denting Panting" ,
+        para : " Lorem iparasum dolor sit amet consectetur adiparaisicing elit."
+    },
+    Custom :{
+        img : 'https://vehiclecare.in/images/service/customrepairs.svg',
+        title : "Custom Repairs" ,
+        para : " Lorem iparasum dolor sit amet consectetur adiparaisicing elit."
+    },
+    Insurance :{
+        img : 'https://vehiclecare.in/images/service/insuranceclaim.svg',
+        title : "Insurances claim" ,
+        para : " Lorem iparasum dolor sit amet consectetur adiparaisicing elit."
+    },
+    pariodic :{
+        img : 'https://vehiclecare.in/images/service/periodicservice.svg',
+        title : "Pariodic Services" ,
+        para : " Lorem iparasum dolor sit amet consectetur adiparaisicing elit."
+    }
+}
+
+let offers = {
+    Nitro :{
+        title:"Nitrogen Gass",
+        montly: "400/Months",
+        years:"4000/year",
+        h:"One Time Payments"
+    },
+    carwsah :{
+        title:"Car Washing",
+        montly: "400/Months",
+        years:"4000/year",
+        h:"One Time Payments"
+    },
+    WheelAlignment :{
+        title:"Wheel Alignment",
+        montly: "400/Months",
+        years:"4000/year",
+        h:"One Time Payments"
+    },
+    Wheel :{
+        title:"Wheel Balnceing",
+        montly: "400/Months",
+        years:"4000/year",
+        h:"One Time Payments"
+    }
+}
+
+let compare = {
+    car:{
+       old:{
+        img:"https://images.unsplash.com/photo-1551836989-b4622a17a792?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bWVyY2VkZXMlMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+       },
+       new:{
+        img:"https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVyY2VkZXMlMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+       }
+    },
+    car1:{
+        old:{
+        img:"https://images.unsplash.com/photo-1551836989-b4622a17a792?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bWVyY2VkZXMlMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        },
+        new:{
+        img:"https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVyY2VkZXMlMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        }
+     },
+     car2:{
+        old:{
+        img:"https://images.unsplash.com/photo-1551836989-b4622a17a792?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bWVyY2VkZXMlMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        },
+        new:{
+        img:"https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bWVyY2VkZXMlMjBjYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+        }
+     }
+}
+
+let buy = {
+    mahindra:{
+        img:"https://images10.gaadi.com/usedcar_image/original/VCC_1553385_24_1658060026.jpg?imwidth=640",
+        brand:"Mahendra",
+        model:2017,
+        Details:"black and XMS",
+        Km:"4500 KM",
+    },
+    tata:{
+        img:"https://images10.gaadi.com/usedcar_image/original/VCC_1553385_24_1658060026.jpg?imwidth=640",
+        brand:"haier",
+        model:2019,
+        Details:"black and XMS",
+        Km:"10000 KM",
+    },
+    hundai:{
+        img:"https://images10.gaadi.com/usedcar_image/original/VCC_1553385_24_1658060026.jpg?imwidth=640",
+        brand:"accent",
+        model:2019,
+        Details:"white",
+        Km:"10000 KM",
+    },
+    toyota:{
+        img:"https://images10.gaadi.com/usedcar_image/original/VCC_1553385_24_1658060026.jpg?imwidth=640",
+        brand:"etios",
+        model:2009,
+        Details:"silver",
+        Km:"170000 KM",
+    },
+}
+
+let a, b,c,d, serviceList ,offerList, compareCarsList, SellCarList, serviceTagsList;
+
+serviceTagsList = document.getElementById('serviceTags');
+serviceList = document.querySelector('#service-list .container .row');
+offerList = document.getElementById('offer-list');
+compareCarsList = document.getElementById('compareCar-list');
+SellCarList = document.getElementById('carList');
+
+
+for(a in CardDetails){
+    serviceTagsList.innerHTML +=`
+    <li>
+        <div class="tags d-flex align-items-center justify-content-around p-1">
+            <img src="${CardDetails[a].img}" alt="" class="me-2">
+            <span>${CardDetails[a].title}</span>
+        </div>
+    </li>
+    `;
+
+    serviceList.innerHTML += `
+    <div class="col-md-3 col-6">
+        <a href="#" class="slot-main text-decoration-none">
+            <div class="slot p-3 mb-4">
+                <div class="slot-icon">
+                    <img src="${CardDetails[a].img}" alt="">
+                </div>
+                <div class="slot-title">
+                    <h4>${CardDetails[a].title}</h4>
+                </div>
+                <div class="slotDetails">
+                    <p>${CardDetails[a].para}</p>
+                </div>
+            </div>
+        </a>
+    </div>
+    `;
+}
+for(b in offers){
+    offerList.innerHTML += `
+    <li>
+        <a href="#">
+            <div class="offercard">
+                <div class="offerTitle">
+                    <h3 class="title">
+                       ${offers[b].title}
+                    </h3>
+                </div>
+                <hr>
+                <div class="offerDetails">
+                    <div class="offerdescription">
+                        <p>${offers[b].montly}</p>
+                        <p>${offers[b].years}</p>
+                        <p>${offers[b].h}</p>
+                    </div>
+                </div>
+                <div class="offerCardControl">
+                    <button class="btn btn-danger" type="button">
+                        Get It Now
+                    </button>
+                </div>
+            </div>
+        </a>
+    </li>
+    `;
+}
+for(c in compare){
+    console.log(compare[c].new.img);
+    compareCarsList.innerHTML += `
+    <li>
+        <div class="img-comp-container">
+            <div class="img-comp-img">
+                <img src="${compare[c].new.img}" width="300" height="200">
+                
+            </div>
+            <div class="img-comp-img img-comp-overlay">
+                <img src="${compare[c].old.img}" width="300" height="200">
+            </div>
+        </div>
+    </li>
+    `;
+}
+for(d in buy){
+    console.log(buy[d]);
+
+    SellCarList.innerHTML +=`
+    <li>
+        <div class="product-card">
+            <div class="product-img">
+                <img src="${buy[d].img}" alt="" class="w-100">
+            </div>
+            <div class="product-details p-2 pb-0">
+                <h1 class="title"><span class="red"><i class="fa fa-car" aria-hidden="true"></i> ${buy[d].brand}</span></h1>
+            
+                <span><i class="fa fa-cog" aria-hidden="true"></i>  ${buy[d].model}</span>
+                <p><i class="fa-solid fa-gas-pump"></i> ${buy[d].Km} <br> KmsDieselAutomatic</p>
+            </div>
+            <div class="product-prise p-2 pt-0">
+                <div class="d-flex justify-content-between align-items-center">
+                    <p class="mt-2"><strong>₹ 22,13,000</strong></p>
+                <div>
+                    <button class="btn btn-outline-danger">Enqury Now</button>
+                </div>
+                </div>
+            </div>
+        </div>
+    </li>  
+    `;
+}
